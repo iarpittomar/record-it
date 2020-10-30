2d63697e723b7e1434619abf65563ab83d79f387
iarpittomar <arpittomar7740@gmail.com>
2020-10-30 15:50:11 +0530

    fix: fix the sorted date order

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -67,7 +67,7 @@ const Calendar: React.FC<IProps> = ({ events, loadUserEvents }) => {
   if (events.length) {
     groupedEvents = groupEventsByDay(events);
     sortedGroupKeys = Object.keys(groupedEvents).sort(
-      (date1, date2) => +new Date(date1) - +new Date(date2)
+      (date1, date2) => +new Date(date2) - +new Date(date1)
     );
   }
   return groupedEvents && sortedGroupKeys ? (
