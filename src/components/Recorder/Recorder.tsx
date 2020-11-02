27fb9f6bd3c62b4fe377c1a7e84ddd6d2dff8cc2
iarpittomar <arpittomar7740@gmail.com>
2020-11-02 16:35:26 +0530

    feat: add play/pause button

--- src/components/Recorder/Recorder.tsx
+++ src/components/Recorder/Recorder.tsx
@@ -1,8 +1,9 @@
-import { Button, Flex, PseudoBox } from '@chakra-ui/core';
+import { Box, Button, Flex, PseudoBox } from '@chakra-ui/core';
 import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { start, stop, selectorDateStart } from '../../redux/recorder.reducer';
 import { createUserEvent } from '../../redux/user-events.reducer';
+import { MdPlayArrow, MdPause } from 'react-icons/md';
 
 const addZero = (num: number) => (num < 10 ? `0${num}` : `${num}`);
 
@@ -11,17 +12,35 @@ const Recorder = () => {
   const dateStart = useSelector(selectorDateStart);
   const started = dateStart !== '';
   let interval = useRef<number>(0);
-  const [, setCount] = useState<number>(0);
+  let [count, setCount] = useState<number>(0);
+  const [isPaused, setPause] = useState<Boolean>(false);
 
   const handleClick = () => {
     if (started) {
       window.clearInterval(interval.current);
       dispatch(createUserEvent());
+      setPause(false);
       dispatch(stop());
     } else {
       dispatch(start());
+      setPause(true);
       interval.current = window.setInterval(() => {
         setCount((count) => count + 1);
+      }, 1000);
+    }
+  };
+
+  const handlePause = () => {
+    let lastCount = count;
+    if (!!isPaused) {
+      setPause(false);
+      window.clearInterval(interval.current);
+    } else {
+      setPause(true);
+      dispatch(start());
+      interval.current = window.setInterval(() => {
+        lastCount = lastCount + 1;
+        setCount(lastCount);
       }, 1000);
     }
   };
@@ -62,6 +81,23 @@ const Recorder = () => {
           width={started ? '22px' : '24px'}
           transition="all 0.3s ease-out"
         ></PseudoBox>
+      </Button>
+      <Button
+        size="md"
+        height="60px"
+        width="60px"
+        bg="#e3f6f5"
+        ml="10px"
+        cursor="pointer"
+        borderRadius="50%"
+        border="0"
+        onClick={handlePause}
+      >
+        {!isPaused ? (
+          <Box as={MdPlayArrow} size="40px" color="green.400"></Box>
+        ) : (
+          <Box as={MdPause} size="40px" color="red.400"></Box>
+        )}
       </Button>
       <PseudoBox m="0 0 0 15px" textAlign="center" width="80px">
         {addZero(hours)}:{addZero(minutes)}:{addZero(seconds)}
