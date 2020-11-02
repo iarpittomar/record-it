21d480e6f50786850333df7c4a6b6272e3069d18
iarpittomar <arpittomar7740@gmail.com>
2020-11-02 12:44:33 +0530

    feat: making calendar component static

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -70,69 +70,164 @@ const Calendar: React.FC<IProps> = ({ events, loadUserEvents }) => {
       (date1, date2) => +new Date(date2) - +new Date(date1)
     );
   }
-  return groupedEvents && sortedGroupKeys ? (
+  // return groupedEvents && sortedGroupKeys ? (
+  //   <Flex direction="row-reverse" m="0 15px" p="20px 0" overflowX="auto">
+  //     {sortedGroupKeys.map((dayKey) => {
+  //       const events = groupedEvents![dayKey];
+  //       const groupDate = new Date(dayKey);
+  //       const day = groupDate.getDate();
+  //       const month = groupDate.toLocaleString(undefined, { month: 'long' });
+  //       return (
+  //         <PseudoBox flex="0 0 300px" p="0 20px">
+  //           <PseudoBox fontSize="16px" m="0 0 15px" textAlign="center">
+  //             <Text
+  //               bg="#bae8e8"
+  //               borderRadius="4px"
+  //               color="#272343"
+  //               display="inline-block"
+  //               fontWeight="bold"
+  //               p="4px 8px"
+  //             >
+  //               {day} {month}
+  //             </Text>
+  //           </PseudoBox>
+  //           <PseudoBox>
+  //             {events.map((event) => {
+  //               return (
+  //                 <Flex
+  //                   key={event.id}
+  //                   justify="space-between"
+  //                   flexWrap="wrap"
+  //                   m="0 0 10px"
+  //                   p="8px 14px"
+  //                   align="flex-start"
+  //                   bg="#e3f6f5"
+  //                   borderRadius="4px"
+  //                 >
+  //                   <PseudoBox flex="1 64%" lineHeight="23px">
+  //                     <Text fontSize="0.8em">10:00 - 12:00</Text>
+  //                     <Text>{event.title}</Text>
+  //                   </PseudoBox>
+  //                   <Button
+  //                     appearance="none"
+  //                     bg="none"
+  //                     border="0"
+  //                     cursor="pointer"
+  //                     flexShrink="initial"
+  //                     height="23px"
+  //                     fontSize="18px"
+  //                     lineHeight="20px"
+  //                     m="0 0 0 10px"
+  //                     p="0"
+  //                     width="23px"
+  //                   >
+  //                     &times;
+  //                   </Button>
+  //                 </Flex>
+  //               );
+  //             })}
+  //           </PseudoBox>
+  //         </PseudoBox>
+  //       );
+  //     })}
+  //   </Flex>
+  // ) : (
+  //   <p>Loading...</p>
+  // );
+  return (
     <Flex direction="row-reverse" m="0 15px" p="20px 0" overflowX="auto">
-      {sortedGroupKeys.map((dayKey) => {
-        const events = groupedEvents![dayKey];
-        const groupDate = new Date(dayKey);
-        const day = groupDate.getDate();
-        const month = groupDate.toLocaleString(undefined, { month: 'long' });
-        return (
-          <PseudoBox flex="0 0 300px" p="0 20px">
-            <PseudoBox fontSize="16px" m="0 0 15px" textAlign="center">
-              <Text
-                bg="#bae8e8"
-                borderRadius="4px"
-                color="#272343"
-                display="inline-block"
-                fontWeight="bold"
-                p="4px 8px"
-              >
-                {day} {month}
-              </Text>
+      <PseudoBox flex="0 0 300px" p="0 20px">
+        <PseudoBox fontSize="16px" m="0 0 15px" textAlign="center">
+          <Text
+            bg="#bae8e8"
+            borderRadius="4px"
+            color="#272343"
+            display="inline-block"
+            fontWeight="bold"
+            p="4px 8px"
+          >
+            16 Nov
+          </Text>
+        </PseudoBox>
+        <PseudoBox>
+          <Flex
+            justify="space-between"
+            flexWrap="wrap"
+            m="0 0 10px"
+            p="8px 14px"
+            align="flex-start"
+            bg="#e3f6f5"
+            borderRadius="4px"
+          >
+            <PseudoBox flex="1 64%" lineHeight="23px">
+              <Text fontSize="0.8em">10:00 - 12:00</Text>
+              <Text>Water the plants</Text>
             </PseudoBox>
-            <PseudoBox>
-              {events.map((event) => {
-                return (
-                  <Flex
-                    key={event.id}
-                    justify="space-between"
-                    flexWrap="wrap"
-                    m="0 0 10px"
-                    p="8px 14px"
-                    align="flex-start"
-                    bg="#e3f6f5"
-                    borderRadius="4px"
-                  >
-                    <PseudoBox flex="1 64%" lineHeight="23px">
-                      <Text fontSize="0.8em">10:00 - 12:00</Text>
-                      <Text>{event.title}</Text>
-                    </PseudoBox>
-                    <Button
-                      appearance="none"
-                      bg="none"
-                      border="0"
-                      cursor="pointer"
-                      flexShrink="initial"
-                      height="23px"
-                      fontSize="18px"
-                      lineHeight="20px"
-                      m="0 0 0 10px"
-                      p="0"
-                      width="23px"
-                    >
-                      &times;
-                    </Button>
-                  </Flex>
-                );
-              })}
+            <Button
+              appearance="none"
+              bg="none"
+              border="0"
+              cursor="pointer"
+              flexShrink="initial"
+              height="23px"
+              fontSize="18px"
+              lineHeight="20px"
+              m="0 0 0 10px"
+              p="0"
+              width="23px"
+            >
+              &times;
+            </Button>
+          </Flex>
+        </PseudoBox>
+      </PseudoBox>
+
+      <PseudoBox flex="0 0 300px" p="0 20px">
+        <PseudoBox fontSize="16px" m="0 0 15px" textAlign="center">
+          <Text
+            bg="#bae8e8"
+            borderRadius="4px"
+            color="#272343"
+            display="inline-block"
+            fontWeight="bold"
+            p="4px 8px"
+          >
+            14 Nov
+          </Text>
+        </PseudoBox>
+        <PseudoBox>
+          <Flex
+            justify="space-between"
+            flexWrap="wrap"
+            m="0 0 10px"
+            p="8px 14px"
+            align="flex-start"
+            bg="#e3f6f5"
+            borderRadius="4px"
+          >
+            <PseudoBox flex="1 64%" lineHeight="23px">
+              <Text fontSize="0.8em">10:00 - 12:00</Text>
+              <Text>Birthday Party</Text>
             </PseudoBox>
-          </PseudoBox>
-        );
-      })}
+            <Button
+              appearance="none"
+              bg="none"
+              border="0"
+              cursor="pointer"
+              flexShrink="initial"
+              height="23px"
+              fontSize="18px"
+              lineHeight="20px"
+              m="0 0 0 10px"
+              p="0"
+              width="23px"
+            >
+              &times;
+            </Button>
+          </Flex>
+        </PseudoBox>
+      </PseudoBox>
     </Flex>
-  ) : (
-    <p>Loading...</p>
   );
 };
 
